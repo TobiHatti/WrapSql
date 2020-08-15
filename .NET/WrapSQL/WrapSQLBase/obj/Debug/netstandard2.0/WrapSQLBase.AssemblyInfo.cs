86524dae63bb5ea5afdd25999f7bf8e9de600414
Tobias Hattinger <tobias.hattinger@gmx.at>
2020-08-15 22:46:11 +0200

    Created NuGet-Package for base-class

--- .NET/WrapSQL/WrapSQLBase/obj/Debug/netstandard2.0/WrapSQLBase.AssemblyInfo.cs
+++ .NET/WrapSQL/WrapSQLBase/obj/Debug/netstandard2.0/WrapSQLBase.AssemblyInfo.cs
@@ -11,13 +11,15 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("WrapSQLBase")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Endev")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("WrapSQLBase")]
+[assembly: System.Reflection.AssemblyCopyrightAttribute("2020 Tobias Hattinger")]
+[assembly: System.Reflection.AssemblyDescriptionAttribute("Base-class for any derivations of WrapSQL. This package has no use by itself.")]
+[assembly: System.Reflection.AssemblyFileVersionAttribute("0.0.1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("0.0.1")]
+[assembly: System.Reflection.AssemblyProductAttribute("WrapSQL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WrapSQLBase")]
-[assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
+[assembly: System.Reflection.AssemblyVersionAttribute("0.0.1.0")]
 
 // Von der MSBuild WriteCodeFragment-Klasse generiert.
 
