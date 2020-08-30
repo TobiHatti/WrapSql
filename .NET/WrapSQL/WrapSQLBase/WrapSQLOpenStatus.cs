02cde153091bec3066f5c973b76282b9a9a2bb3a
Tobias Hattinger <tobias.hattinger@gmx.at>
2020-08-30 22:50:10 +0200

    Added WrapSQLOpenStatus enum

---
+++ .NET/WrapSQL/WrapSQLBase/WrapSQLOpenStatus.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WrapSQL
+{
+    public enum OpenStatus
+    {
+        Success = 0,
+        ConnectionError = 500,
+        DatabaseBusy = 501,
+        GeneralError = 555,
+    }
+}
